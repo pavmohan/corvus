c0278813592c1f303515a582761e09b836d6e81e
LCmry <lcrees.2014@gmail.com>
2017-04-30 15:05:58 -0700

    Basic request and responses

--- cheesediary/app.py
+++ cheesediary/app.py
@@ -1,30 +1,8 @@
 from webob import Request, Response
 from webob import exc
+from webob.dec import wsgify
 
-from cheese import Cheese
-
-def controller(cls):
-	def application(environ, start_response):
-		req = Request(environ)
-		try:
-			instance = cls(req, **req.urlvars)
-			action = req.urlvars.get('action')
-			if action:
-				action += '_' + req.method.lower()
-			else:
-				action = req.method.lower()
-			try:
-				method = getattr(instance, action)
-			except AttributeError:
-				raise exc.HTTPNotFound("No action %s" % action)
-			resp = method()
-			if isinstance(resp, basestring):
-				resp = Response(body=resp)
-		except exc.HTTPException, e:
-			resp = e
-		return resp(environ, start_response)
-	return application
-
-
-req = Request.blank('/')
-app = req.get_response(controller(Cheese))
+@wsgify
+def app(req):
+	resp = Response(body='index')
+	return resp
--- cheesediary/cheese.py
+++ cheesediary/cheese.py
@@ -1,10 +1,26 @@
 # Controller for Cheeses
+from webob import Request, Response
+from webob import exc
+from webob.dec import wsgify
+import re
 
-class Cheese(object):
+class Cheese():
 
-	def __init__(self, req):
-		self.request = req
+	def index(self):
+		return 'This is the index for cheeses'
 
-	def get(self):
-		return 'This is the index'
+	def get(self, cheese_id):
+		return 'Cheese number %s' % cheese_id
 
+@wsgify
+def app(req):
+	cheese = Cheese()
+	regex = re.compile('/cheese/\d')
+	if (req.path == '/cheeses'):
+		resp = Response(body=cheese.index())
+	elif (re.match(regex, req.path)):
+		object_id = re.findall("[0-9]+", req.path)[-1]
+		resp = Response(body=cheese.get(object_id))
+	else:
+		resp = Response(body='not found')
+	return resp
